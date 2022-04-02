236b47a7214c63a924b019689d3a47f86f11d27a
Hiếu <hieu.trantrung1204@gmail.com>
2022-04-02 18:54:28 +0700

    feat(Lint): add flutter_lint

--- modules/design_system/test/design_system_test.dart
+++ modules/design_system/test/design_system_test.dart
@@ -1,6 +1,5 @@
-import 'package:flutter_test/flutter_test.dart';
-
 import 'package:design_system/design_system.dart';
+import 'package:flutter_test/flutter_test.dart';
 
 void main() {
   test('adds one to input values', () {
