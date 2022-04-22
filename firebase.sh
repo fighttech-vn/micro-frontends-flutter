8071e535138263a04c4abb78f857cb2971967b19
trunghieu <trunghieu@trunghieu.local>
2022-04-22 14:30:04 +0700

    feat(Dev): update script

--- firebase.sh
+++ firebase.sh
@@ -12,4 +12,15 @@ flutter build web --release --dart-define=FLUTTER_WEB_USE_SKIA=true
 # flutter build web --web-renderer=html
 firebase deploy
 
+cd modules/storybook
+
+if test -f "$FILE"; then
+    rm $FILE
+fi
+
+flutter pub get
+
+flutter build web --release --dart-define=FLUTTER_WEB_USE_SKIA=true
+firebase deploy
+cd ../..
 echo "Deoloy firebase Done !!!"
